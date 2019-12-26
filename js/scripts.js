c1b25f9f6583f2409ecdbfc3b5ef63b107a45930
Sudhir Deolalikar <sudhir.deolalikar@vuclip.com>
2019-12-26 23:01:12 +0530

    Hotel Listing

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
-/*
- * Title:   Travelo - Travel, Tour Booking HTML5 Template - Custom Javascript file
- * Author:  http://themeforest.net/user/soaptheme
- */
 
 tjq(document).ready(function() {
-    // UI Form Element
+  tjq("#price-range").slider({
+    range: true,
+    min: 0,
+    max: 1000,
+    values: [0, 1000],
+    slide: function(event, ui) {
+      tjq(".min-price").html("$" + ui.values[0]);
+      tjq(".max-price").html("$" + ui.values[1]);
+    }
+  });
+  tjq(".min-price").value("$" + tjq("#price-range").slider("values", 0));
+  tjq(".max-price").value("$" + tjq("#price-range").slider("values", 1));
 });
