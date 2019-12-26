d9a02ca54348a6b7e36e5531dee058bf94086423
Sudhir Deolalikar <sudhir.deolalikar@vuclip.com>
2019-12-26 23:07:00 +0530

    Hotel Listing

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-
 tjq(document).ready(function() {
-  tjq("#price-range").slider({
-    range: true,
-    min: 0,
-    max: 1000,
-    values: [0, 1000],
-    slide: function(event, ui) {
-      tjq(".min-price").html("$" + ui.values[0]);
-      tjq(".max-price").html("$" + ui.values[1]);
-    }
-  });
-  tjq(".min-price").value("$" + tjq("#price-range").slider("values", 0));
-  tjq(".max-price").value("$" + tjq("#price-range").slider("values", 1));
+  if (tjq("#price-range").length > 0) {
+    tjq("#price-range").slider({
+      range: true,
+      min: 0,
+      max: 1000,
+      values: [0, 1000],
+      slide: function(event, ui) {
+        tjq(".min-price").html("$" + ui.values[0]);
+        tjq(".max-price").html("$" + ui.values[1]);
+      }
+    });
+    tjq(".min-price").value("$" + tjq("#price-range").slider("values", 0));
+    tjq(".max-price").value("$" + tjq("#price-range").slider("values", 1));
+  }
 });
